40d3e29e00cf113a1c0ab626a08d7af8e77f238a
Artur Dias <arturdrm@hotmail.com>
2024-10-28 14:29:44 -0300

    added: Login service implementation (client)

--- GlobalWeather.Client/Services/UserService.cs
+++ GlobalWeather.Client/Services/UserService.cs
@@ -38,11 +38,32 @@ internal class UserService(
         }
     }
 
-    public Task<ResultModel<string>> LoginAsync(
+    public async Task<ResultModel<string>> LoginAsync(
         string email,
         string password,
         CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var result = await httpClient.PostAsJsonAsync(
+                "login",
+                new LoginModel
+                {
+                    Email = email.Trim(),
+                    Password = password.Trim(),
+                },
+                cancellationToken);
+
+            result.EnsureSuccessStatusCode();
+
+            var content = await result.Content.ReadFromJsonAsync<ResultModel<string>>(cancellationToken);
+
+            return content ?? ResultModel<string>.ErrorResult("Could not read response");
+        }
+        catch (Exception e)
+        {
+            logger.LogError("Error on login. Error: {error}", e.ToString());
+            return ResultModel<string>.ErrorResult("Internal Server Error");
+        }
     }
 }

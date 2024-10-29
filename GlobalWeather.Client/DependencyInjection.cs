db3563f3a21b20580fe11328ca232ebca9bbb572
Artur Dias <arturdrm@hotmail.com>
2024-10-29 09:50:49 -0300

    added: Client dependency injection

---
+++ GlobalWeather.Client/DependencyInjection.cs
@@ -0,0 +1,45 @@
+using GlobalWeather.Client.Services;
+using GlobalWeather.Shared.Contracts;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace GlobalWeather.Client;
+
+internal static class DependencyInjection
+{
+    private const string LocalServerUrl = "https://localhost:44336/";
+    private const string RemoteServerUrl = "https://g7dyc2vjj5.execute-api.us-east-1.amazonaws.com/Prod/";
+
+    private static string GetServerUrl(IWebAssemblyHostEnvironment environment)
+    {
+        return environment.IsDevelopment()
+            ? LocalServerUrl
+            : RemoteServerUrl;
+    }
+
+    public static IServiceCollection AddClientServices(
+        this IServiceCollection services,
+        IWebAssemblyHostEnvironment environment)
+    {
+        services.AddHttpClient<ICountryService, CountryService>(client =>
+        {
+            var url = GetServerUrl(environment) + "api/v1/country/";
+            client.BaseAddress = new Uri(url);
+        });
+        services.AddHttpClient<IUserService, UserService>(client =>
+        {
+            var url = GetServerUrl(environment) + "api/v1/user/";
+            client.BaseAddress = new Uri(url);
+        });
+        services.AddHttpClient<IWeatherService, WeatherService>(client =>
+        {
+            var url = GetServerUrl(environment) + "api/v1/weather/";
+            client.BaseAddress = new Uri(url);
+        });
+
+        return services
+            .AddScoped<AuthService>()
+            .AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>()
+            .AddAuthorizationCore();
+    }
+}
--- GlobalWeather.Client/Program.cs
+++ GlobalWeather.Client/Program.cs
@@ -2,31 +2,15 @@ using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using GlobalWeather.Client;
-using GlobalWeather.Client.Services;
-using GlobalWeather.Shared.Contracts;
 using MudBlazor.Services;
-using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
 builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddScoped<AuthService>();
-builder.Services.AddHttpClient<ICountryService, CountryService>(client =>
-{
-    client.BaseAddress = new Uri("https://localhost:44336/api/v1/country/");
-});
-builder.Services.AddHttpClient<IUserService, UserService>(client =>
-{
-    client.BaseAddress = new Uri("https://localhost:44336/api/v1/user/");
-});
-builder.Services.AddHttpClient<IWeatherService, WeatherService>(client =>
-{
-    client.BaseAddress = new Uri("https://localhost:44336/api/v1/weather/");
-});
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
-builder.Services.AddAuthorizationCore();
+builder.Services.AddClientServices(builder.HostEnvironment);
 
 await builder.Build().RunAsync();
